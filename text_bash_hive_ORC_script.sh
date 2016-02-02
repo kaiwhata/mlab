6acb938635cc68c217d6fdb25461c46b21a44c70
Elf Eldridge <kaiwhata@gmail.com>
2016-02-02 17:07:34 +1300

    Update text_bash_hive_ORC_script.sh

--- text_bash_hive_ORC_script.sh
+++ text_bash_hive_ORC_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#run with ./text_bash_hive_ORC_script_v2.sh <directory_containing_gzipped_files>
+
 #write current  size of hdfs directory to log file
-sudo du -s -h /tmp/hadoop-fogbank > tmp_logfile.txt
+du -s /tmp > tmp_logfile.txt
 
 #readin reference variables list and turn into reference array
 getArray() {
@@ -15,7 +17,7 @@ getArray() {
 }
 
 
-NOT_CREATED_TABLE="false"
+NOT_CREATED_TABLE="true"
 DIRECTORY=$1
 echo $DIRECTORY
 #take user argument for directory and return list of all files in that directory with echo
@@ -24,8 +26,9 @@ do
   echo "$entry"
   FILENAME=$entry
   #then we cycle through them gunzipping each one in turn
-  echo `du $FILENAME` 
-  echo `gunzip $FILENAME`
+ echo `du $FILENAME` 
+#  no-longer require unzipping
+# echo `gunzip $FILENAME`
 
   #Report File size of zipped and unzipped version
   echo  `du ${FILENAME:0:(-3)}`
@@ -37,13 +40,16 @@ do
   TABLENAME="temp${FILENAME:(-8):(-7)}"
 
   #Create appropriate header file to input into hive for table creation
-  du ${FILENAME:0:(-3)} >> tmp_logfile.txt
-  echo "$TABLENAME" >> tmp_logfile.txt
+  #du ${FILENAME:0:(-3)} >> tmp_logfile.txt
+  #echo "$TABLENAME" >> tmp_logfile.txt
 
-  FIELDS=`head -1 "${FILENAME:0:(-3)}" |tr ".[]" "___"|sed "s/\t/\n/g" |sed "s/$/ STRING/g"|paste -s -d","`;
+  #FIELDS=`head -1 "${FILENAME:0:(-3)}" |tr ".[]" "___"|sed "s/\t/\n/g" |sed "s/$/ STRING/g"|paste -s -d","`;
   #echo $FIELDS
-  echo "$FIELDS" > ~/fields_list.txt
+  #echo "$FIELDS" > ~/fields_list.txt
 
+  FIELDS=`cat ~/fields_list.txt`
+  echo "Fields are:"
+  echo "$FIELDS"
   NEWFIELDS=""
 
   #now we cycle through fields an convert non-STRING variables to those listed in variable_types.txt
@@ -76,40 +82,46 @@ do
   STARTTIME=`date +"%s"` 
   #date +"%s ">> tmp_logfile.txt
 
+  #hive commands 
+  #echo `hive -e "Use Mlab; ${COMMAND_STRING}; LOAD DATA LOCAL INPATH '${CSV_NAME}' INTO TABLE mlab.${TABLENAME};"`
   #hive command using beeline rather than hive CLI
-#  echo `beeline -u jdbc:hive2:// -e "Use Mlab; ${COMMAND_STRING}; LOAD DATA LOCAL INPATH '${CSV_NAME}' INTO TABLE mlab.${TABLENAME};"`
+  echo `beeline -u jdbc:hive2:// -e "Use Mlab; ${COMMAND_STRING}; LOAD DATA LOCAL INPATH '${FILENAME}' INTO TABLE mlab.${TABLENAME};"`
 
   #maximum hdfs filesize
   du -s /tmp >> tmp_logfile.txt
 
-  #formatting as ORC
+  #formatting as parquet
+  #PAR_COMMAND_STRING="CREATE TABLE mlab.${TABLENAME}_par (${FIELDS}) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS PARQUET TBLPROPERTIES('skip.header.line.count'='1');"
+
   #format for the above but using single table
-  ORC_COMMAND_STRING="CREATE TABLE mlab.temp_orc (${FIELDS}) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS PARQUET TBLPROPERTIES('skip.header.linder.line.count'='1');"
+  PAR_COMMAND_STRING="CREATE TABLE mlab.temp_paro (${FIELDS}) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' STORED AS orc TBLPROPERTIES('orc.compress.size'='8192', 'orc.compress'='SNAPPY' ,'skip.header.linder.line.count'='1');"
 
-  #if the temp_ORC table doesn't exist we must create it
-#  if [ "$NOT_CREATED_TABLE" == "true" ]; then
+  #if the temp_par table doesn't exist we must create it
+  if [ "$NOT_CREATED_TABLE" == "true" ]; then
        #now we attempt to put everything inb ONE table rather than creating new parquet tables for each day
-       echo `beeline -u jdbc:hive2:// -e "Use Mlab; ${ORC_COMMAND_STRING}; INSERT INTO TABLE mlab.temp_orc SELECT * from mlab.${TABLENAME};"`
+       echo `beeline -u jdbc:hive2:// -e "Use Mlab; ${PAR_COMMAND_STRING}; INSERT INTO TABLE mlab.temp_paro SELECT * from mlab.${TABLENAME};"`
        echo "Table Created"
-#       NOT_CREATED_TABLE="false"
-#  else
-#       echo `beeline -u jdbc:hive2:// -e "Use Mlab; INSERT INTO TABLE mlab.temp_orc SELECT * from mlab.${TABLENAME};"`
-#       echo  "Table appended"
-#  fi
+       NOT_CREATED_TABLE="false"
+  else
+       echo `beeline -u jdbc:hive2:// -e "Use Mlab; INSERT INTO TABLE mlab.temp_paro SELECT * from mlab.${TABLENAME};"`
+       echo  "Table appended"
+  fi
 
-  #Delete original table
-#  echo `beeline -u jdbc:hive2:// -e "Use Mlab; DROP TABLE mlab.${TABLENAME};"`
+  #remove original csv table
+  #echo `hive -e "Use Mlab; DROP TABLE mlab.${TABLENAME};"`
+  #now using beeline instead of hive CLI
+  echo `beeline -u jdbc:hive2:// -e "Use Mlab; DROP TABLE mlab.${TABLENAME};"`
 
   #Loadin data time
   ENDTIME=`date +"%s"` 
   #date  +"%s" >> tmp_logfile.txt
 
   #readin new size of /tmp file size
-  du ${FILENAME:0:(-3)} >> tmp_logfile.txt
-  $TABLENAME >> tmp_logfile.txt
+  #du ${FILENAME:0:(-3)} >> tmp_logfile.txt
+  #$TABLENAME >> tmp_logfile.txt
   expr $ENDTIME - $STARTTIME >> tmp_logfile.txt
   du -s /tmp >> tmp_logfile.txt
 
   #Remove original csv file (.gz file removed by gunzip)
-  echo `rm ${FILENAME:0:(-3)}`
+#  echo `rm ${FILENAME}`
 done
